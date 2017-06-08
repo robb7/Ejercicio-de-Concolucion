9445173f469086c8f745fb72dda9b004eb29d35e
Robb <robonline7@gmail.com>
2017-06-07 22:54:28 -0500

    Calc

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function calConvolucionPeriodica(){
+
+  var x = document.convPeriodica.operandoX.value;
+  var y = document.convPeriodica.operandoY.value;
+  var result = parseInt(x) + parseInt(y);
+  document.convPeriodica.resultado.value = result;
+}
