5c6a31c22bea5f3d1030d34411249b1f2afde09d
Robb <robonline7@gmail.com>
2017-06-12 01:29:06 -0500

    Avanze JS

--- js/index.js
+++ js/index.js
@@ -1,7 +1,50 @@
 function calConvolucionPeriodica(){
 
-  var x = document.convPeriodica.operandoX.value;
-  var y = document.convPeriodica.operandoY.value;
-  var result = parseInt(x) + parseInt(y);
-  document.convPeriodica.resultado.value = result;
+  //Obtener los números de entrada.
+  var x_input = document.convPeriodica.operandoX.value;
+  var y_input = document.convPeriodica.operandoY.value;
+  //Creamos los arrays para hacer las operaciones
+  var x_array = new Array();
+  var y_array = new Array();
+  //Separamos los datos de entrada  por "," y los guardamos
+  //en el array
+
+  var x_array = x_input.split(',');
+  var y_array = y_input.split(',');
+
+  //Calculamos la Longitud
+  var xlenght = x_array.length;
+  var ylenght = y_array.length;
+
+  //Array Multidimensiona
+  var result_array = new Array();
+
+  for (w =0; w < xlenght; w++) // Altura y
+  {
+   result_array[w]=new Array(); // Ancho x
+   for (j=0; j < ylenght; j++)
+   {
+    result_array[w][j]=0;
+   }
+  }
+
+  //Multiplicacion de Columnas
+  for (var d = 0; d < xlenght; d++) {
+    for (var i = 0; i < ylenght; i++)
+    {
+          result_array[d][i] = x_array[d] * y_array[i];
+    }
+  }
+
+ for (var z = 1; z < ylenght; z++)
+ {
+   for (var m = 0; i < z;  m++)
+   {
+     result_array.unshift(0);
+   }
+ }
+
+  //Test de Salida
+  document.getElementById("test").innerHTML =  "x= "+ x_array+"    y= "+ y_array;
+  document.convPeriodica.resultado.value = result_array;
 }
