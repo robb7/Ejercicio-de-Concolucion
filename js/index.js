6e399b2045fdd9e196efe49b2914a13aac8a2920
Robb <robonline7@gmail.com>
2017-06-14 19:22:17 -0500

    Funcion terminada

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-  function calConvolucionPeriodica(){
+function calConvolucionPeriodica(){
 
   //Obtener los números de entrada.
   var x_input = document.convPeriodica.operandoX.value;
@@ -12,9 +12,11 @@
   var x_array = x_input.split(',');
   var y_array = y_input.split(',');
 
-  //Calculamos la Longitud
+  //Longitud de Variables
   var xlenght = x_array.length ;
-  var ylenght = y_array.length ;
+  var ylenght = y_array.length *2;
+  var restlenght = y_array.length;
+
 
   //Array Multidimensiona
   var mult_array = new Array();
@@ -22,7 +24,7 @@
   //Multiplicacion de Columnas
   var n=0;
   var z= 0;
-  //
+
 
   for (var x = 0; x < xlenght; x++)
   {
@@ -34,7 +36,12 @@
             n++;
             z++;
           }
+
           mult_array[n] = y_array[y]*x_array[x];
+          if(isNaN(mult_array[n]))
+          {
+            mult_array[n] = 0;
+          }
           n++;
     }
           z = 0;
@@ -43,19 +50,35 @@
 
   //Array Resultado
   var resultado = new Array(ylenght);
-  var f=0;
+
+
+  // Sumar los valores de la matriz
   for (var i = 0; i < ylenght; i++)
   {
-    for(var q = 0; q < multlenght; q++)
-    f= q * ylenght +1;
-    resultado[0]= mult_array[q].value +resultado[0].value;
+    var indi=i;
+    var sumando = 0;
+    for (var d = 0; d < xlenght ; d++)
+    {
+        sumando = mult_array[indi] + sumando;
+        indi = ylenght + indi+d;
+    }
+
+    resultado[i] = sumando;
   }
 
 
+
+
+
+
+
+
   //Test de Salida
-  document.getElementById("test").innerHTML =  "x= "+ x_array+"    y= "+ y_array;
+  document.getElementById("test").innerHTML =  "x= "+ x_array+"Longitud x="+ xlenght+ "y= "+ y_array+ " longitud Y= "+
+  ylenght+"Longitud = "+ mult_array.length + "Cadena= "+ mult_array[6];
   //Array completo
-   document.convPeriodica.resultado.value =   resultado;
+  //document.convPeriodica.resultado.value =  mult_array;
+  document.convPeriodica.resultado.value =  resultado;
   //celda en especifico
   // document.convPeriodica.resultado.value = result_array[1][0];
 
